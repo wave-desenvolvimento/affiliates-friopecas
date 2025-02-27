18aa0d550c88a5d6b9e3996ca7a18bfc54967e78
wesley moraes <wesleymoraesserafim@gmail.com>
2025-02-27 14:46:25 -0300

    fix

--- node/__tests__/unit/resolvers/getAffiliates.spec.ts
+++ node/__tests__/unit/resolvers/getAffiliates.spec.ts
@@ -1,4 +1,4 @@
-import type { QueryGetAffiliatesArgs } from 'vtex.affiliates'
+import type { QueryGetAffiliatesArgs } from 'friopecas.affiliates'
 
 import { getAffiliates } from '../../../resolvers/getAffiliates'
 
